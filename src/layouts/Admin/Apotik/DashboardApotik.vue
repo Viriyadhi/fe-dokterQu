2e586006c7c39601b40acc597f9bd708ad91db7c
Filberg Giovanno <vanixgzy@gmail.com>
2023-03-09 10:29:28 +0700

    Feat: [TF] SideBar Admin Dokter & Apotik

---
+++ src/layouts/Admin/Apotik/DashboardApotik.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app>
+        <SideBarApotik :items="routes()"/>
+        <v-main>
+            <router-view></router-view>
+        </v-main>
+    </v-app>
+</template>
+<script>
+import {ApotikAdmin} from '@/router/admin'
+// import AppBarAdmin from '@/components/Admin/SideBarApotik.vue';
+import SideBarApotik from '@/components/Admin/SideBarApotik.vue';
+export default {
+    data: () => ({ drawer: false }),
+    methods: {
+        routes() {
+            return ApotikAdmin.children;
+        }
+    },
+    components: { SideBarApotik }
+}
+</script>
+
+<style scoped>
+.v-list-item {
+    margin-top: 200px;
+}
+p {
+}
+</style>
