beafd21a71d8fc6dc51f16ee97e5f6c930998176
Viriyadhi <viriyadhitjan@gmail.com>
2023-04-12 07:22:20 +0700

    fix:[VS] optimize UI and fix routing

--- src/components/E-commerce/ECommerceNav.vue
+++ src/components/E-commerce/ECommerceNav.vue
@@ -1,129 +1,78 @@
 <template>
-  <div>
-    <v-card  >
-      <v-app-bar
-        :style="this.appbarStyle"
-        fixed
-        class="px-sm-11 navbar pt-3"
-        elevation="0"
-        v-scroll="onScrollContent"
+  <v-card>
+    <v-app-bar
+      :style="this.appbarStyle"
+      fixed
+      class="px-sm-11 navbar pt-3"
+      elevation="0"
+      v-scroll="onScrollContent"
+    >
+      <v-btn icon color="black">
+        <router-link :to="{ name: 'Default' }">
+          <v-icon>mdi-arrow-left</v-icon>
+        </router-link>
+      </v-btn>
+      <h4 class="title-web" v-if="isXSmallScreenSize">Keluar Toko</h4>
+
+      <v-spacer></v-spacer>
+
+      <v-text-field
+        class="mt-6"
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        solo
+        rounded
+        dense
+      ></v-text-field>
+      <v-spacer></v-spacer>
+
+      <div
+        v-if="!localStorage && isXSmallScreenSize"
+        class="d-flex align-center justify-end flex-grow-1"
       >
-        <v-btn icon color="black">
-          <router-link :to="{ name: 'Default' }">
-            <v-icon>mdi-arrow-left</v-icon>
-          </router-link>
-        </v-btn>
-        <h4 class="title-web" v-if="isXSmallScreenSize">Keluar Toko</h4>
-
-        <v-spacer></v-spacer>
-
-        <v-text-field
-          class="mt-6"
-          prepend-inner-icon="mdi-magnify"
-          label="Search"
-          solo
-          rounded
-          dense
-        ></v-text-field>
-        <v-spacer></v-spacer>
-
-        <div
-          v-if="!localStorage && isXSmallScreenSize"
-          class="d-flex align-center justify-end flex-grow-1"
-        >
-          <router-link :to="{ name: 'Login' }">
-            <a class="login-text mr-8">LOGIN</a>
-          </router-link>
-          <div class="text-center mx-8">
-            <v-btn
-              :to="{ name: 'Register' }"
-              rounded
-              color="success"
-              class="button-register"
-              dark
+        <router-link :to="{ name: 'Login' }">
+          <a class="login-text mr-8">LOGIN</a>
+        </router-link>
+        <div class="text-center mx-8">
+          <v-btn
+            :to="{ name: 'Register' }"
+            rounded
+            color="success"
+            class="button-register"
+            dark
+          >
+            <router-link :to="{ name: 'Register' }">
+              <a class="register-text">REGISTER</a>
+            </router-link>
+          </v-btn>
+        </div>
+      </div>
+      <div
+        v-if="localStorage"
+        class="d-flex align-center justify-end flex-grow-1"
+      >
+        <div class="text-center">
+          <router-link :to="{ name: 'ProfileHome' }">
+            <v-chip
+              v-bind="attrs"
+              v-on="on"
+              outlined
+              color="black"
+              class="mr-8"
             >
-              <router-link :to="{ name: 'Register' }">
-                <a class="register-text">REGISTER</a>
-              </router-link>
-            </v-btn>
-          </div>
+              <v-avatar>
+                <v-img :src="localStorage.data.photo"></v-img>
+              </v-avatar>
+              <span class="ml-2">Hi, {{ localStorage.data.name }}</span>
+            </v-chip>
+          </router-link>
         </div>
-        <div
-          v-if="localStorage"
-          class="d-flex align-center justify-end flex-grow-1"
-        >
-          <div class="text-center">
-            <v-dialog v-model="dialog" width="500">
-              <template v-slot:activator="{ on, attrs }">
-                <div class="d-flex flex-row align-center px-3" style="gap: 16px">
-                  <router-link :to="{ name: 'CartView' }">
-                    <v-btn icon color="black">
-                      <v-badge color="green" :content="cartItems">
-                        <v-icon> mdi-cart</v-icon>
-                      </v-badge>
-                    </v-btn>
-                  </router-link>
-
-                  <v-chip
-                    v-if="isXSmallScreenSize"
-                    v-bind="attrs"
-                    v-on="on"
-                    outlined
-                    color="black"
-                  >
-                    <v-avatar>
-                      <v-img
-                        src="https://cdn.vuetifyjs.com/images/lists/1.jpg"
-                      ></v-img>
-                    </v-avatar>
-                    <span class="ml-2">Hi, {{ localStorage.data.name }}</span>
-                  </v-chip>
-                </div>
-              </template>
-
-              <v-card>
-                <v-card-title class="text-h5 grey lighten-2">
-                  Privacy Policy
-                </v-card-title>
-
-                <v-card-text>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </v-card-text>
-
-                <v-divider></v-divider>
-
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-
-                  <v-btn color="primary" @click="dialog = false" text>
-                    Gajadi
-                  </v-btn>
-
-                  <v-btn color="primary" text @click="logout()">
-                    I accept
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </div>
-        </div>
-        <div class="d-flex flex-row" style="gap: 12px">
-          <v-divider vertical style="height: 40px"></v-divider>
-          <v-icon class="mdi-config align-self-center">mdi-cog</v-icon>
-        </div>
-      </v-app-bar>
-      <v-main class="pa-0 mt-16">
-        <router-view />
-      </v-main>
-    </v-card>
-  </div>
+      </div>
+    </v-app-bar>
+    <v-main class="pa-0 mt-16">
+      <router-view />
+    </v-main>
+  </v-card>
 </template>
 <script>
 import axios from "axios";
